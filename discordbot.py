aec7bd19c8ac61728b988e8333188e457e115592
close012 <zhi_xian_loo@hotmail.com>
2025-07-12 21:24:32 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -17,10 +17,6 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
-async def on_read():
-    print("Online")
-
-@bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
